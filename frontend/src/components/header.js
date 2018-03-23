0ba701fec5cc23300559088d25d4da63e70eec07
Smithface <kylewibblesmith@gmail.com>
2018-03-23 15:32:17 -0400

    changed alignment of navLinks

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import './header.css';
 
 class Header extends Component {
   getLinks() {
@@ -23,7 +24,7 @@ class Header extends Component {
 
   render() {
     return (
-      <div>
+      <div className="navBar" >
         <Link to="/">Main Page</Link>
         <ul>{this.getLinks()}</ul>
       </div>
