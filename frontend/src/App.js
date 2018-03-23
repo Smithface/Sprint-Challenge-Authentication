4ee7a5036cf7421a169119320764aa3aaf37a96f
Smithface <kylewibblesmith@gmail.com>
2018-03-23 13:54:06 -0400

    added new logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/header';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
